fc4246dd46e1777f689d8a26cddd8f36eca17ee3
shyamayadav154 <shyamayadav154@gmail.com>
2022-04-12 19:33:45 +0530

    small fix

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -1,43 +1,36 @@
-import React, { useState, useEffect,useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import ResultCard from '../components/ResultCard'
 import { motion } from 'framer-motion'
-
 
 const Add = () => {
   const [query, setQuery] = useState('')
   const [results, setResults] = useState([])
   const inputRef = useRef(null)
 
-   const fetchSearch =() =>{
- fetch(
-   `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`
- )
-   .then((res) => res.json())
-   .then((data) => {
-     if (!data.errors) {
-       setResults(data.results)
-     } else {
-       setResults([])
-     }
- 
-   })
-      }
-
+  const fetchSearch = () => {
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${query}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data.errors) {
+          setResults(data.results)
+        } else {
+          setResults([])
+        }
+      })
+  }
 
   const onChange = (e) => {
     e.preventDefault()
     setQuery(e.target.value)
 
-
-     
     fetchSearch()
-
   }
 
-
-  useEffect(()=>{
+  useEffect(() => {
     inputRef.current.focus()
-  },[])
+  }, [])
   return (
     <motion.div
       initial={{ opacity: 0 }}
